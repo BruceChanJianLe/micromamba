c13ae0b9eedb2018aaca43e722c54d55491783b3
Chan Jian Le <chanjl@i2r.a-star.edu.sg>
2025-10-22 22:09:47 +0800

    wip: add nix flake file to install micromamba

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+  description = "Development environment with micromamba";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs?ref=release-24.05";
+  };
+
+  outputs = { self, nixpkgs }:
+    let
+      systems = [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ];
+      forAllSystems = f: nixpkgs.lib.genAttrs systems (system: f {
+        pkgs = import nixpkgs { inherit system; };
+      });
+    in
+    {
+      devShells = forAllSystems ({ pkgs }: {
+        default = pkgs.mkShell {
+          buildInputs = [
+            pkgs.micromamba
+          ];
+
+          shellHook = ''
+            echo "Micromamba development environment"
+            echo "Micromamba version: $(micromamba --version)"
+            # echo ""
+            # echo "To initialize micromamba, run:"
+            # echo "  eval \"\$(micromamba shell hook --shell bash)\""
+            # echo ""
+            # echo "Then you can create environments with:"
+            # echo "  micromamba create -n myenv python=3.11"
+            # echo "  micromamba activate myenv"
+          '';
+        };
+      });
+    };
+}
